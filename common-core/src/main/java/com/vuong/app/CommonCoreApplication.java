47d2f1a5d991f07563769b5904d9a36b1d9ea3ef
Vuong Tran Minh <vuongtranminh.hn@gmail.com>
2024-03-25 14:55:12 +0700

    mod: config build package module

--- common-core/src/main/java/com/vuong/app/CommonCoreApplication.java
+++ common-core/src/main/java/com/vuong/app/CommonCoreApplication.java
@@ -1,13 +1,13 @@
-package com.vuong.app;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CommonCoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommonCoreApplication.class, args);
-	}
-
-}
+//package com.vuong.app;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class CommonCoreApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(CommonCoreApplication.class, args);
+//	}
+//
+//}
--- common-core/src/test/java/com/vuong/app/CommonCoreApplicationTests.java
+++ common-core/src/test/java/com/vuong/app/CommonCoreApplicationTests.java
@@ -1,13 +1,13 @@
-package com.vuong.app;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CommonCoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.vuong.app;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CommonCoreApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
